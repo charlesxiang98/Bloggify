8cd1b66f366a606ba1a42fcb3a2a1a05e8a7e12a
Charles <xhb@xhb.home>
2020-05-11 18:16:57 -0400

    project

---
+++ src/main/java/com/site/blog/my/core/config/Constants.java
@@ -0,0 +1,6 @@
+package com.site.blog.my.core.config;
+
+
+public class Constants {
+    public final static String FILE_UPLOAD_DIC = "/opt/deploy/upload/";//uploading file prefix, modify according to need
+}
